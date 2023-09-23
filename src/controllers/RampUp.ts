36f966e056188baca181f870cd4b8a02edbaa31e
jonathandow <dow3@purdue.edu>
2023-09-23 16:10:42 -0400

    Added RampUp Metric Calculation

---
+++ src/controllers/RampUp.ts
@@ -0,0 +1,50 @@
+import {
+    fetchRepositoryContributors,
+    fetchRepositoryStars,
+    fetchRepositoryForks,
+    fetchFirstCommitTime,
+  } from '../utils/RampUpAPI';
+  
+  const calculateRampUp = async (owner: string, repo: string) => {
+    try {
+      // Fetch data from GitHub API
+      const contributors = await fetchRepositoryContributors(owner, repo);
+      const stars = await fetchRepositoryStars(owner, repo);
+      const forks = await fetchRepositoryForks(owner, repo);
+      const firstCommitTime = await fetchFirstCommitTime(owner, repo);
+  
+      // Sample weights
+      const weights = {
+        Contributors: 0.3,
+        Stars: 0.2,
+        Forks: 0.2,
+        FirstCommit: 0.3,
+      };
+  
+      // Calculate the ramp-up score
+      let rampUpScore =
+        weights.Contributors * contributors.length +
+        weights.Stars * stars.length +
+        weights.Forks * forks.length;
+  
+      if (firstCommitTime) {
+        // Calculate the time it took for the first commit in milliseconds
+        const currentTime = new Date().getTime();
+        const firstCommitTimestamp = new Date(firstCommitTime).getTime();
+        const timeDifference = currentTime - firstCommitTimestamp;
+  
+        // Normalize the time difference (0 to 1) and add it to the ramp-up score
+        const maxTimeDifference = 365 * 24 * 60 * 60 * 1000; // Max time difference set to 1 year
+        const normalizedTimeDifference = Math.min(timeDifference / maxTimeDifference, 1);
+        rampUpScore += weights.FirstCommit * normalizedTimeDifference;
+      }
+  
+      console.log('Ramp-Up Score:', rampUpScore);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  const owner = "fill in later"
+  const repo = "fill in later"
+  calculateRampUp(owner, repo);
+  
